3abb576ce041ede720fb18c4a4973074332b1a72
DESKTOP-2FOS94E\仲本和子 <zhongbenhuiwu@gmail.com>
2021-12-09 16:50:54 +0900

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,69 @@
-$(function(){
-    $('.btn_box').on('mouseover',function(){
-        // $('.border')
-    });
+
+$(function() {
 
 
+$(function() {
+
+    let s1=1;
+
+    $('.humberger').on('click', function() {
+
+       s1=s1+1;
+       console.log(s1)
+       if(s1%2==0){
+
+       
+    
+    
+   
+
+        
+            $('.one').addClass('add1');
+            $('.two').addClass('add2');
+            $('.three').addClass('add3');
+            
+        
+            $('.sp').css('opacity', 0);
+
+            $('.header-right').css('opacity',1)
+        
+
+
+       }else{ 
+        $('.one').removeClass('add1');
+        $('.two').removeClass('add2');
+        $('.three').removeClass('add3');
+        
+    
+        $('.sp').css('opacity', 1);
+
+        $('.header-right').css('opacity',0)
+    
+
+
+       }
+
+
+
+
+    
+    
+
+      
+    })
+
+
+
+})
+
+//  $(function(){
+
+//      $('.humberger').on('click',function()
+//      {
+//          $('.humberger').removeclass('zero');
+//      })
+//  })
+
+
+  
 });
